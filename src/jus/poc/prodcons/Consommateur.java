4d6d0b2d400efbe791e893920b72abd1bafec992
bastien-7 <bastien.terrier7@gmail.com>
2017-12-01 09:10:38 +0100

    classes

---
+++ src/jus/poc/prodcons/Consommateur.java
@@ -0,0 +1,17 @@
+package jus.poc.prodcons;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
+			int deviationTempsDeTraitement) throws ControlException {
+		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
