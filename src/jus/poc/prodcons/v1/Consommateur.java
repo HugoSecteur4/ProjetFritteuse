0a74ea01699bf3ce4fef301b55d49034aeda4e58
gmDefault <cedric.lafrasse@gmail.com>
2017-12-09 17:34:54 +0100

    Aléa conso

--- src/jus/poc/prodcons/v1/Consommateur.java
+++ src/jus/poc/prodcons/v1/Consommateur.java
@@ -1,6 +1,7 @@
 package jus.poc.prodcons.v1;
 
 import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
 import jus.poc.prodcons.ControlException;
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons._Consommateur;
@@ -11,11 +12,15 @@ public class Consommateur extends Acteur implements _Consommateur {
 //	
 //	protected int nb_Conso = 0;
 	
+	private Aleatoire alea;
+	
+	
 	protected Consommateur(int type, Observateur observateur, int moyenneTempsDeTraitement,
 			int deviationTempsDeTraitement) throws ControlException {
 		super(type, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
 //		this.ID_Conso = nb_Conso;
 //		nb_Conso ++;
+		alea = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
 	}
 
 	@Override
