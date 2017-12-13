52f24289da1c57c80a6f2fe7b2b9a3870b7679a5
bastien-7 <bastien.terrier7@gmail.com>
2017-12-13 10:55:35 +0100

    v3 cree mais vide

---
+++ src/jus/poc/prodcons/v3/Consommateur.java
@@ -0,0 +1,78 @@
+package jus.poc.prodcons.v3;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons.v3.ProdCons;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	private Aleatoire temps_consommation;
+	protected int NbMessageConso = 0;
+	ProdCons Buff;
+
+	protected Consommateur(Observateur observateur, ProdCons buffer, int moyenneTempsDeTraitement,
+			int deviationTempsDeTraitement) throws ControlException {
+		super(Acteur.typeConsommateur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		temps_consommation = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		this.Buff = buffer;
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return NbMessageConso;
+	}
+
+	public void run() {
+		int nummessage = 0;
+		MessageX m;
+		while (!Buff.production_terminee() || Buff.enAttente() != 0) {
+			
+			try {
+				if (Math.random()<=0.5) {
+					yield();
+					System.out.println("COMMUTATION");
+				}
+				m = (MessageX) Buff.get(this);
+				nummessage = m.getNumero_message();
+				if (Math.random()<=0.5) {
+					yield();
+					System.out.println("COMMUTATION");
+				}
+				System.out.println("***************************Traitement du message par le consommateur : " +this.identification() +", message : " + m.toString());
+				System.out.println();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			try {
+				if (Math.random()<=0.5) {
+					yield();
+					System.out.println("COMMUTATION");
+				}
+				Thread.sleep(temps_consommation.next());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				
+			}
+			if (Math.random()<=0.5) {
+				yield();
+				System.out.println("COMMUTATION");
+			}
+			// Si on récupère un message :
+
+			NbMessageConso++;
+		}
+		if (Math.random()<=0.5) {
+			yield();
+			System.out.println("COMMUTATION");
+		}
+		//System.out.println("consommation terminée, nbmessageconso : " + this.NbMessageConso + " message de fin : " + nummessage + " prod terminee : " + Buff.production_terminee() + " en attente : " + Buff.enAttente());
+	}
+
+	public String toString() {
+		return "Consommateur" + identification();
+	}
+}
---
+++ src/jus/poc/prodcons/v3/TestProdCons.java
@@ -0,0 +1,235 @@
+package jus.poc.prodcons.v3;
+
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Map;
+import java.util.Properties;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+
+public class TestProdCons extends Simulateur{
+
+	private int nbProd = 0;
+	private int nbCons = 0;
+	private int nbBuffer = 0;
+	private int tempsMoyenProduction = 0;
+	private int deviationTempsMoyenProduction = 0;
+	private int tempsMoyenConsommation = 0;
+	private int deviationTempsMoyenConsommation = 0;
+	private int nombreMoyenDeProduction = 0;
+	private int deviationNombreMoyenDeProduction = 0;
+	private int nombreMoyenNbExemplaire = 0;
+	private int deviationNombreMoyenNbExemplaire = 0;
+	
+	public int getNbProd() {
+		return nbProd;
+	}
+
+
+
+	public int getNbCons() {
+		return nbCons;
+	}
+
+
+
+	public void setNbCons(int nbCons) {
+		this.nbCons = nbCons;
+	}
+
+
+
+	public int getNbBuffer() {
+		return nbBuffer;
+	}
+
+
+
+	public void setNbBuffer(int nbBuffer) {
+		this.nbBuffer = nbBuffer;
+	}
+
+
+
+	public int getTempsMoyenProduction() {
+		return tempsMoyenProduction;
+	}
+
+
+
+	public void setTempsMoyenProduction(int tempsMoyenProduction) {
+		this.tempsMoyenProduction = tempsMoyenProduction;
+	}
+
+
+
+	public int getDeviationTempsMoyenProduction() {
+		return deviationTempsMoyenProduction;
+	}
+
+
+
+	public void setDeviationTempsMoyenProduction(int deviationTempsMoyenProduction) {
+		this.deviationTempsMoyenProduction = deviationTempsMoyenProduction;
+	}
+
+
+
+	public int getTempsMoyenConsommation() {
+		return tempsMoyenConsommation;
+	}
+
+
+
+	public void setTempsMoyenConsommation(int tempsMoyenConsommation) {
+		this.tempsMoyenConsommation = tempsMoyenConsommation;
+	}
+
+
+
+	public int getDeviationTempsMoyenConsommation() {
+		return deviationTempsMoyenConsommation;
+	}
+
+
+
+	public void setDeviationTempsMoyenConsommation(int deviationTempsMoyenConsommation) {
+		this.deviationTempsMoyenConsommation = deviationTempsMoyenConsommation;
+	}
+
+
+
+	public int getNombreMoyenDeProduction() {
+		return nombreMoyenDeProduction;
+	}
+
+
+
+	public void setNombreMoyenDeProduction(int nombreMoyenDeProduction) {
+		this.nombreMoyenDeProduction = nombreMoyenDeProduction;
+	}
+
+
+
+	public int getDeviationNombreMoyenDeProduction() {
+		return deviationNombreMoyenDeProduction;
+	}
+
+
+
+	public void setDeviationNombreMoyenDeProduction(int deviationNombreMoyenDeProduction) {
+		this.deviationNombreMoyenDeProduction = deviationNombreMoyenDeProduction;
+	}
+
+
+
+	public int getNombreMoyenNbExemplaire() {
+		return nombreMoyenNbExemplaire;
+	}
+
+
+
+	public void setNombreMoyenNbExemplaire(int nombreMoyenNbExemplaire) {
+		this.nombreMoyenNbExemplaire = nombreMoyenNbExemplaire;
+	}
+
+
+
+	public int getDeviationNombreMoyenNbExemplaire() {
+		return deviationNombreMoyenNbExemplaire;
+	}
+
+
+
+	public void setDeviationNombreMoyenNbExemplaire(int deviationNombreMoyenNbExemplaire) {
+		this.deviationNombreMoyenNbExemplaire  = deviationNombreMoyenNbExemplaire;
+	}
+
+
+
+	public void setNbProd(int nbProd) {
+		this.nbProd = nbProd;
+	}
+
+
+
+	/**
+	* Retreave the parameters of the application.
+	* @param file the final name of the file containing the options.
+	*/
+	protected void init(String file) {
+	// retreave the parameters of the application
+	
+	/*final class Properties extends java.util.Properties {
+		Class<?> thisOne = getClass();
+	private static final long serialVersionUID = 1L;
+	public int get(String key){return Integer.parseInt(getProperty(key));}
+	public Properties(String file) {
+	try{
+	loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+	}catch(Exception e){e.printStackTrace();}
+	}
+	}
+	Properties option = new Properties("jus/poc/prodcons/options/"+file);
+	// Integer.parseInt(option.getProperty("nbProd"));
+	*/
+		System.out.println("initialisation");
+		Properties properties = new Properties();
+		
+		try {
+			
+			properties.loadFromXML(getClass().getResourceAsStream(file));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		String key;
+		int value;
+		Class<?> thisOne = getClass();
+		for(Map.Entry<Object,Object> entry : properties.entrySet()) {
+		key = (String)entry.getKey();
+		value = Integer.parseInt((String)entry.getValue());
+		try {
+			thisOne.getDeclaredField(key).set(this,value);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+		}
+	
+	}
+	
+	public TestProdCons(Observateur observateur) {
+		super(observateur);
+	}
+
+	@Override
+	protected void run() throws Exception {
+		this.init("../options/option.xml");
+		ProdCons buffer = new ProdCons(nbBuffer, this.nbProd);
+		
+		for (int i =0; i<nbProd;i++){
+			(new Producteur(observateur,buffer, tempsMoyenProduction, deviationTempsMoyenProduction, nombreMoyenDeProduction, deviationNombreMoyenDeProduction)).start();
+		}
+		
+		for (int i =0; i<nbCons;i++){
+			Consommateur consommateur = new Consommateur(observateur,buffer, tempsMoyenConsommation, deviationTempsMoyenConsommation);
+			consommateur.start();
+		}
+
+
+		
+
+
+		
+	}
+
+	public static void main(String[] args){new TestProdCons(new Observateur()).start();}
+	
+}
